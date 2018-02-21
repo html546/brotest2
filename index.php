0c0229ea1ebc8f8d4542d8e4d484fac09ee87d11
html546 <1776787598@qq.com>
2018-02-21 19:52:53 +0800

    for语句循环遍历数组

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
 
-$wage = array(
-  "市场部"=>array(
-      array(1,"高某","市场部经理",5000.00),
-      array(2,"洛某","职员",3000.00),
-      array(3,"峰某","职员",3000.00)
-  ),
-    "产品部"=>array(
-        array(1,"李某","产品部经理",6000.00),
-        array(2,"周某","职员",4000.00),
-        array(3,"吴某","职员",3200.00)
-    ),
-    "财务部"=>array(
-        array(1,"郑某","财务部经理",4500.00),
-        array(2,"王某","职员",2000.00),
-        array(3,"冯某","职员",1500.00)
-    )
-);
-print_r($wage["市场部"]);
-print_r($wage["市场部"][1]);
-print_r($wage["市场部"][1][3]);
+$contact = array(1,"高某","A公司","北京市","(010)98765432","gao@brophp.com");
+
+echo "<table border='1' width='600' align='center'>";
+echo "<caption><h1>联系人列表</h1></caption>";
+echo "<tr bgcolor='#ddd'>";
+echo "<th>编号</th><th>姓名</th><th>公司</th><th>地址</th><th>电话</th><th>邮箱</th>";
+echo "</tr><tr>";
+for ($i = 0;$i<count($contact);$i++){
+    echo "<td>".$contact[$i]."</td>";
+}
+echo "</tr>";
+
+echo "</table>";
