149185eb47d89304903ae3f2dc4c2f4defd81b96
html546 <1776787598@qq.com>
2018-02-21 16:23:26 +0800

    借助call_user_func_array函数自定义回调函数

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
 
-function filter($fun){
-    for ($i = 0;$i<=100;$i++){
-        if($fun($i)){
-            continue;
-        }
-        echo $i."<br/>";
-    }
+/**
+ * @param $msg1
+ * @param $msg2
+ */
+function fun($msg1,$msg2){
+    echo "\$msg1 = ".$msg1;
+    echo '<br/>';
+    echo "\$msg2 = ".$msg2;
 }
 
-function one($sum){
-    return $sum % 3 === 0;
-}
-
-function two($sum){
-    return $sum == strrev($sum);
-}
-
-filter('one');
-
-echo '-------------------------<br/>';
-
-filter('two');
+call_user_func_array('fun',array('LAMP','兄弟连'));
