88872d69511dabf5796ec75ab7b83ccc679eac81
html546 <1776787598@qq.com>
2018-02-21 20:34:29 +0800

    foreach语句遍历数组2

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <?php
 
-$contact = array(1,14=>"高某","A公司","北京市",14=>"(010)98765432","gao@brophp.com");
+$wage = array(
+    "市场部"=>array(
+        array(1,"高某","市场部经理",5000.00),
+        array(2,"洛某","职员",3000.00),
+        array(3,"峰某","职员",3000.00)
+    ),
+    "产品部"=>array(
+        array(1,"李某","产品部经理",6000.00),
+        array(2,"周某","职员",4000.00),
+        array(3,"吴某","职员",3200.00)
+    ),
+    "财务部"=>array(
+        array(1,"郑某","财务部经理",4500.00),
+        array(2,"王某","职员",2000.00),
+        array(3,"冯某","职员",1500.00)
+    )
+);
 
-$sum = 0;
-foreach ($contact as $value){
-    echo "在数组\$contact中第".$sum."元素是:".$value."<br/>";
-    $sum++;
+foreach ($wage as $selector=>$table){
+    echo "<table border='1' width='600' align='center'>";
+    echo "<caption><h1>".$selector."10月份工资表</h1></caption>";
+    echo "<tr bgcolor='#ddd'><th>编号</th><th>姓名</th><th>职务</th><th>工资</th></tr>";
+        foreach ($table as $row){
+            echo "<tr>";
+                foreach ($row as $col){
+                    echo "<td>".$col."</td>";
+                }
+            echo "</tr>";
+        }
+
+
+    echo "</table>";
 }
