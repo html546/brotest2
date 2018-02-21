e5e5d33d071f56b627cc81f2c3cd99a3315ee922
html546 <1776787598@qq.com>
2018-02-21 08:54:32 +0800

    全局变量3

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $two = 100;
  * 不能在函数中直接使用全局变量
  */
 function demo(){
-    global $one,$two; //使用global关键字就可以加载全局变量在函数中使用
-    echo "运行结果:".($one+$two)."<br/>";
+    $GLOBALS['two'] = $GLOBALS['one'] + $GLOBALS['two']; //使用超全局变量加载全局变量在函数中使用
 }
 demo();
+echo $two;
