e039259239615a677d9e78f5bdcb0c9d8bbaa985
html546 <1776787598@qq.com>
2018-02-21 09:03:51 +0800

    静态变量

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
-$one = 200;
-$two = 100;
-
-/**
- * 不能在函数中直接使用全局变量
- */
-function demo(){
-    $GLOBALS['two'] = $GLOBALS['one'] + $GLOBALS['two']; //使用超全局变量加载全局变量在函数中使用
+function test(){
+    static $a = 0;
+    echo $a;
+    $a++;
 }
-demo();
-echo $two;
+test();
+test();
+test();
