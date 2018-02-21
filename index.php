6913681b4d816ea25335efee6e4daf115271ca59
html546 <1776787598@qq.com>
2018-02-21 16:30:24 +0800

    借助call_user_func_array()来调用回调函数

--- index.php
+++ index.php
@@ -11,3 +11,23 @@ function fun($msg1,$msg2){
 }
 
 call_user_func_array('fun',array('LAMP','兄弟连'));
+
+function filter($fun){
+    for ($i = 0;$i<100;$i++){
+        if(call_user_func_array($fun,array($i))){
+            continue;
+        }
+        echo $i."<br/>";
+    }
+}
+
+function one($sum){
+    return $sum % 3 == 0;
+}
+
+function two($sum){
+    return $sum == strrev($sum);
+}
+filter('one');
+echo "-------------<br/>";
+filter('two');
