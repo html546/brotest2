71fd00a941349196316e37c82d30b8c2b2fbab00
html546 <1776787598@qq.com>
2018-02-21 20:08:57 +0800

    使用foreach语句遍历数组

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 
-$contact = array(
-    array(1,"高某","A公司","北京市","(010)98765432","gao@brophp.com"),
-    array(2,"洛某","B公司","上海市","(021)12345678","lm@apache.com"),
-    array(3,"峰某","C公司","天津市","(022)24680246","fm@mysql.com"),
-    array(4,"书某","D公司","重庆市","(023)13579135","sm@php.com"),
-);
+$contact = array(1,14=>"高某","A公司","北京市",14=>"(010)98765432","gao@brophp.com");
 
-echo "<table border='1' width='600' align='center'>";
-echo "<caption><h1>联系人列表</h1></caption>";
-echo "<tr bgcolor='#ddd'>";
-echo "<th>编号</th><th>姓名</th><th>公司</th><th>地址</th><th>电话</th><th>邮箱</th>";
-echo "</tr>";
-for ($i = 0;$i<count($contact);$i++){
-    echo "<tr>";
-    for ($j = 0;$j<count($contact[$i]);$j++){
-        echo "<td>".$contact[$i][$j]."</td>";
-    }
-    echo "</tr>";
+$sum = 0;
+foreach ($contact as $value){
+    echo "在数组\$contact中第".$sum."元素是:".$value."<br/>";
+    $sum++;
 }
-echo "</table>";
