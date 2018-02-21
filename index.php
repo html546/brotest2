87f7cf7af0f180a9fb3b320fd1b2999759947ab0
html546 <1776787598@qq.com>
2018-02-21 17:37:13 +0800

    闭包函数

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
-/**
- * 匿名函数
- * @param $params
- */
-$fun = function($params){
-  echo $params;
-};
 
-$fun('www.ydma.com');
-
-
-/***
- * 调用回调函数将匿名函数作为参数传入
- * @param $callback
- */
 function callback($callback){
     $callback();
 };
 
-callback(function(){
-   echo "闭包函数测试";
+$var = '字符串';
+
+callback(function() use (&$var){
+   echo "闭包函数传参数测试{$var}";
 });
