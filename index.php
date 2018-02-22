cf820ca0d232bdf736573bd87ea8ba00a866ae08
html546 <1776787598@qq.com>
2018-02-22 21:26:33 +0800

    $_GET

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-
-foreach ($_SERVER as $key=>$value){
-    echo '$_SERVER['.$key.'] = '.$value.'<br/>';
-}
-
-/*echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";*/
-
-echo $_SERVER['REMOTE_ADDR'];
+ echo "用户添加的联系人信息如下:<br/>";
+ foreach ($_POST as $key=>$value){
+     echo $key.":".$value."<br/>";
+ }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>添加联系人</title>
+</head>
+<body>
+<form action="" method="post">
+    编号: <input type="text" name="id"><br>
+    姓名: <input type="text" name="name"><br>
+    公司: <input type="text" name="company"><br>
+    地址: <input type="text" name="address"><br>
+    电话: <input type="text" name="phone"><br>
+    EMAIL: <input type="text" name="email"><br>
+    <input type="submit" value="追加新联系人">
+</form>
+</body>
+</html>
