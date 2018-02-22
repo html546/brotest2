d809164e431176919d9c2f8d04f9759d8f15722f
html546 <1776787598@qq.com>
2018-02-22 22:30:41 +0800

    array_reverse

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 
 $lamp = array("os"=>"Linux","WebServer"=>"Apache","Database"=>"MySQL","Language"=>"PHP");
-print_r(array_flip($lamp));
-
-$trans = array("a"=>1,"b"=>1,"c"=>2);
-print_r(array_flip($trans));
+print_r(array_reverse($lamp));
