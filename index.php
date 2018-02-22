811617f441fe146b7c033d8c46356ac2ec7eedf5
html546 <1776787598@qq.com>
2018-02-22 09:23:15 +0800

    list函数2

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 
-$info = array('coffee','brown','caffeine');
+$contact = array("ID"=>1,"姓名"=>"高某","公司"=>"A公司","地址"=>"北京市");
 
-list($drink,$color,$power) = $info;
+list($key,$value) = each($contact);
 
-echo "$drink is $color and $power makes it special.\n";
-
-list($drink,,$power) = $info;
-
-echo "$drink has $power.\n";
-
-list(,,$power) = $info;
-
-echo "I need $power.\n";
+echo "$key =>$value";
 
 
 
