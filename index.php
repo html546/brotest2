ad8002121366a14c75982b00e21046291036ca17
html546 <1776787598@qq.com>
2018-02-22 09:27:06 +0800

    用while改写foreach循环

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 
-$contact = array("ID"=>1,"姓名"=>"高某","公司"=>"A公司","地址"=>"北京市");
+$contact = array(
+  "ID"=>1,
+  "姓名"=>"高某",
+    "公司"=>"A公司",
+    "地址"=>"北京市",
+    "电话"=>"(010)98765432",
+    "EMAIL"=>"gao@brophp.com"
+);
 
-list($key,$value) = each($contact);
-
-echo "$key =>$value";
-
-
-
-
+echo "<dl>一个联系人信息:";
+while (list($key,$value)=each($contact)){
+    echo "<dd>$key:$value</dd>";
+}
+echo "</dl>";
