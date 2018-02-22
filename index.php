e00741146d1e8aa62ee00a3fb92587a67afa79bb
html546 <1776787598@qq.com>
2018-02-22 20:18:40 +0800

    $_SERVER

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
 <?php
 
-$contact = array(
-  "ID"=>1,
-  "姓名"=>"高某",
-    "公司"=>"A公司",
-    "地址"=>"北京市",
-    "电话"=>"(010)98765432",
-    "EMAIL"=>"gao@brophp.com"
-);
+foreach ($_SERVER as $key=>$value){
+    echo '$_SERVER['.$key.'] = '.$value.'<br/>';
+}
 
-echo "第一个元素:".key($contact)."=>".current($contact)."<br/>";
-echo "第一个元素:".key($contact)."=>".current($contact)."<br/>";
-next($contact);
-next($contact);
-echo "第三个元素:".key($contact)."=>".current($contact)."<br/>";
-end($contact);
-echo "最后一个元素:".key($contact)."=>".current($contact)."<br/>";
-prev($contact);
-echo "最后第二个元素:".key($contact)."=>".current($contact)."<br/>";
-reset($contact);
-echo "又回到了第一个元素:".key($contact)."=>".current($contact)."<br/>";
+/*echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";*/
+
+echo $_SERVER['REMOTE_ADDR'];
